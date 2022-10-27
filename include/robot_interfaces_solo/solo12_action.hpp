475d2e25fd130878d81ba8c15fe207e395e7722c
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-10-27 16:57:13 +0200

    Implement Loggable for Solo12Action/Observation

--- include/robot_interfaces_solo/solo12_action.hpp
+++ include/robot_interfaces_solo/solo12_action.hpp
@@ -15,30 +15,13 @@
 
 namespace robot_interfaces_solo
 {
-// FIXME: implement robot_interfaces::Loggable
-struct Solo12Action
+struct Solo12Action : public robot_interfaces::Loggable
 {
     Vector12d joint_torques = Vector12d::Zero();
     Vector12d joint_positions = Vector12d::Zero();
     Vector12d joint_velocities = Vector12d::Zero();
     Vector12d joint_position_gains = Vector12d::Zero();
     Vector12d joint_velocity_gains = Vector12d::Zero();
-    double heart_beat;  // TODO: what is this?  currently unused
-
-    template <class Archive>
-    void serialize(Archive& archive)
-    {
-        archive(
-
-            CEREAL_NVP(joint_torques),
-            CEREAL_NVP(joint_positions),
-            CEREAL_NVP(joint_velocities),
-            CEREAL_NVP(joint_position_gains),
-            CEREAL_NVP(joint_velocity_gains),
-            CEREAL_NVP(heart_beat)
-
-        );
-    }
 
     /**
      * @brief Create a zero-torque action
@@ -49,6 +32,63 @@ struct Solo12Action
     static Solo12Action Zero()
     {
         return Solo12Action();
+    }
+
+    template <class Archive>
+    void serialize(Archive& archive)
+    {
+        archive(
+
+            CEREAL_NVP(joint_torques),
+            CEREAL_NVP(joint_positions),
+            CEREAL_NVP(joint_velocities),
+            CEREAL_NVP(joint_position_gains),
+            CEREAL_NVP(joint_velocity_gains)
+
+        );
+    }
+
+    std::vector<std::string> get_name() override
+    {
+        return {"joint_torques",
+                "joint_positions",
+                "joint_velocities",
+                "joint_position_gains",
+                "joint_velocity_gains"};
+    }
+
+    std::vector<std::vector<double>> get_data() override
+    {
+        // first map the Eigen vectors to std::vectors
+        std::vector<double> torque;
+        torque.resize(joint_torques.size());
+        Vector12d::Map(&torque[0], joint_torques.size()) = joint_torques;
+
+        std::vector<double> position;
+        position.resize(joint_positions.size());
+        Vector12d::Map(&position[0], joint_positions.size()) = joint_positions;
+
+        std::vector<double> velocity;
+        velocity.resize(joint_velocities.size());
+        Vector12d::Map(&velocity[0], joint_velocities.size()) =
+            joint_velocities;
+
+        std::vector<double> position_gains;
+        position_gains.resize(joint_position_gains.size());
+        Vector12d::Map(&position_gains[0], joint_position_gains.size()) =
+            joint_position_gains;
+
+        std::vector<double> velocity_gains;
+        velocity_gains.resize(joint_velocity_gains.size());
+        Vector12d::Map(&velocity_gains[0], joint_velocity_gains.size()) =
+            joint_velocity_gains;
+
+        // then return them in a fixed size vector of vectors to avoid
+        // copying due to pushing back value of information!
+        std::vector<std::vector<double>> result;
+        result = {torque, position, velocity, position_gains, velocity_gains};
+
+        return result;
     }
 };
 }  // namespace robot_interfaces_solo
--- include/robot_interfaces_solo/solo12_observation.hpp
+++ include/robot_interfaces_solo/solo12_observation.hpp
@@ -15,8 +15,7 @@
 
 namespace robot_interfaces_solo
 {
-// FIXME: implement robot_interfaces::Loggable
-struct Solo12Observation
+struct Solo12Observation : public robot_interfaces::Loggable
 {
     // joints data
     Vector12d joint_positions;
@@ -42,6 +41,7 @@ struct Solo12Observation
     void serialize(Archive& archive)
     {
         archive(
+
             CEREAL_NVP(joint_positions),
             CEREAL_NVP(joint_velocities),
             CEREAL_NVP(joint_torques),
@@ -56,8 +56,102 @@ struct Solo12Observation
             CEREAL_NVP(num_lost_command_packets),
             CEREAL_NVP(num_sent_sensor_packets),
             CEREAL_NVP(num_lost_sensor_packets)
+
         );
+    }
+
+    std::vector<std::string> get_name() override
+    {
+        return {"joint_positions",
+                "joint_velocities",
+                "joint_torques",
+                "joint_target_torques",
+                "joint_encoder_index",
+                "slider_positions",
+                "imu_accelerometer",
+                "imu_gyroscope",
+                "imu_linear_acceleration",
+                "imu_attitude",
+                "num_sent_command_packets",
+                "num_lost_command_packets",
+                "num_sent_sensor_packets",
+                "num_lost_sensor_packets"};
+    }
+
+    std::vector<std::vector<double>> get_data() override
+    {
+        // first map the Eigen vectors to std::vectors
+
+        std::vector<double> _joint_positions;
+        _joint_positions.resize(joint_positions.size());
+        Vector12d::Map(&_joint_positions[0], joint_positions.size()) =
+            joint_positions;
+
+        std::vector<double> _joint_velocities;
+        _joint_velocities.resize(joint_velocities.size());
+        Vector12d::Map(&_joint_velocities[0], joint_velocities.size()) =
+            joint_velocities;
+
+        std::vector<double> _joint_torques;
+        _joint_torques.resize(joint_torques.size());
+        Vector12d::Map(&_joint_torques[0], joint_torques.size()) =
+            joint_torques;
+
+        std::vector<double> _joint_target_torques;
+        _joint_target_torques.resize(joint_target_torques.size());
+        Vector12d::Map(&_joint_target_torques[0], joint_target_torques.size()) =
+            joint_target_torques;
+
+        std::vector<double> _joint_encoder_index;
+        _joint_encoder_index.resize(joint_encoder_index.size());
+        Vector12d::Map(&_joint_encoder_index[0], joint_encoder_index.size()) =
+            joint_encoder_index;
+
+        std::vector<double> _slider_positions;
+        _slider_positions.resize(slider_positions.size());
+        Eigen::Vector4d::Map(&_slider_positions[0], slider_positions.size()) =
+            slider_positions;
+
+        std::vector<double> _imu_accelerometer;
+        _imu_accelerometer.resize(imu_accelerometer.size());
+        Eigen::Vector3d::Map(&_imu_accelerometer[0], imu_accelerometer.size()) =
+            imu_accelerometer;
+
+        std::vector<double> _imu_gyroscope;
+        _imu_gyroscope.resize(imu_gyroscope.size());
+        Eigen::Vector3d::Map(&_imu_gyroscope[0], imu_gyroscope.size()) =
+            imu_gyroscope;
+
+        std::vector<double> _imu_linear_acceleration;
+        _imu_linear_acceleration.resize(imu_linear_acceleration.size());
+        Eigen::Vector3d::Map(&_imu_linear_acceleration[0],
+                             imu_linear_acceleration.size()) =
+            imu_linear_acceleration;
+
+        std::vector<double> _imu_attitude;
+        _imu_attitude.resize(imu_attitude.size());
+        Eigen::Vector4d::Map(&_imu_attitude[0], imu_attitude.size()) =
+            imu_attitude;
+
+        // then return them in a fixed size vector of vectors to avoid
+        // copying due to pushing back value of information!
+        std::vector<std::vector<double>> result;
+        result = {_joint_positions,
+                  _joint_velocities,
+                  _joint_torques,
+                  _joint_target_torques,
+                  _joint_encoder_index,
+                  _slider_positions,
+                  _imu_accelerometer,
+                  _imu_gyroscope,
+                  _imu_linear_acceleration,
+                  _imu_attitude,
+                  {num_sent_command_packets},
+                  {num_lost_command_packets},
+                  {num_sent_sensor_packets},
+                  {num_lost_sensor_packets}};
+
+        return result;
     }
 };
 }  // namespace robot_interfaces_solo
-
