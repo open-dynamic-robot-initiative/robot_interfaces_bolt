868bcf814233fbe21eb08739132baeb4a643cf17
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2022-11-14 09:03:12 +0100

    doc: Add docstrings to solo12_show_data

--- scripts/solo12_show_data.py
+++ scripts/solo12_show_data.py
@@ -17,11 +17,29 @@ from robot_interfaces_solo import solo12
 
 
 class Robot:
+    """Wrapper around the robot interface for running in different control modes.
+
+    Before using the robot, :meth:`initialize` needs to be called to start the robot.
+    Then :meth:`update` should be called in a loop at 1 kHz to run the controller.
+    While running, the control mode can be changed at any time, using
+    :meth:`set_control_mode`.
+    """
+
     class ControlMode(enum.Enum):
+        """The different control modes supported by the :class:`Robot` class."""
+
+        #: Apply zero torque on all joints.
         ZERO_TORQUE = 1
+        #: Use position controller to hold all joints at their current position.
         HOLD_POSITION = 2
 
     def __init__(self, config_file: str, fake_robot: bool = False) -> None:
+        """
+        Args:
+            config_file:  Path to the driver config file.
+            fake_robot:  If true, use the fake backend instead of the real robot (for
+                testing).
+        """
         self.kp = 3.0
         self.kd = 0.05
         self.control_mode = Robot.ControlMode.HOLD_POSITION
@@ -37,6 +55,7 @@ class Robot:
         self.t = 0
 
     def initialize(self):
+        """Initialize the robot.  Needs to be called before :meth:`update`."""
         # Initializes the robot (e.g. performs homing).
         self.robot_backend.initialize()
 
@@ -56,6 +75,11 @@ class Robot:
         self.desired_action.joint_velocity_gains = np.array([self.kd] * 12)
 
     def update(self):
+        """Get robot data and send a new command to the robot.
+
+        This method is expected to be called in the control loop.  The command that is
+        sent depends on the selected control mode (see :meth:`set_control_mode`).
+        """
         # get data
         obs = self.robot_frontend.get_observation(self.t)
         applied_action = self.robot_frontend.get_applied_action(self.t)
@@ -67,6 +91,10 @@ class Robot:
         return obs, status, applied_action
 
     def set_control_mode(self, mode: ControlMode):
+        """Set the control mode.
+
+        See :class:`ControlMode` for the available modes.
+        """
         self.control_mode = mode
 
         if self.control_mode == Robot.ControlMode.ZERO_TORQUE:
@@ -82,31 +110,65 @@ class Robot:
 
 
 class SliderBar(u.ProgressBar):
+    """urwid widget to show the position of a slider via a progress bar."""
+
     def __init__(self, label):
+        """
+        Args:
+            label: Label that is shown next to the value (e.g. the slider's index).
+        """
         super().__init__(None, "progressbar.complete", 0.0, 1.0)
 
         self.label = label
         self.slider_position = 0
 
     def set_position(self, position):
+        """Set position of the slider.
+
+        Args:
+            position: Slider position in range [0, 1].
+        """
         self.slider_position = position
         self.set_completion(position)
 
     def get_text(self):
+        """Text that is shown on the progress bar (override of urwid.ProgressBar)."""
         return f"slider {self.label}: {self.slider_position:.4f}"
 
 
 def labeled_vector(label: str, vector: typing.Sequence, fmt: str = "% .4f") -> str:
+    """Construct string with the label followed by the values of the vector.
+
+    Example:
+
+    .. code-block:: Python
+
+        labeled_vector("Label", [1, 2, 3])
+        -> "Label: 1.0000 2.0000 3.0000"
+
+    Args:
+        label:  Label put before the values
+        vector:  Sequence of values
+        fmt:  Conversion specifier for formatting the values of vector.
+
+    Returns:
+        The constructed string (see example above).
+    """
     n = len(vector)
     fmt_str = "%s " + " ".join([fmt] * n)
     return fmt_str % (label, *vector)
 
 
 class Window:
+    """Manages the TUI.
+
+    urwid-based TUI for showing the robot data and setting the control mode.
+    """
+
     def __init__(self, robot: Robot):
         self.robot = robot
 
-        # define all the text fields
+        # define all the text fields (so they can be updated later)
         self.obs_motor_data_text = u.Text("")
         self.obs_imu_acc_texts = u.Text("")
         self.obs_imu_gyro_texts = u.Text("")
@@ -211,24 +273,29 @@ class Window:
                 self.set_control_mode(Robot.ControlMode.ZERO_TORQUE)
 
     def set_control_mode(self, mode: Robot.ControlMode):
+        """Set robot control mode and update the corresponding label in the TUI."""
         self.robot.set_control_mode(mode)
         self.control_mode_text.set_text(f"Active Control Mode: {mode.name}")
 
     def run(self) -> None:
+        """Run the application
+
+        Runs the TUI mainloop which also takes care of updating the robot controller.
+        """
         update_interval_s = 0.001
 
         self.set_control_mode(Robot.ControlMode.HOLD_POSITION)
 
         # set update callback
-        def update_window(loop: u.MainLoop, win):
-            win.update_data()
+        def update_window(loop: u.MainLoop, win: Window):
+            win.update()
             loop.set_alarm_in(update_interval_s, update_window, win)
 
         self.loop.set_alarm_in(update_interval_s, update_window, self)
         self.loop.run()
 
-    def update_data(self) -> None:
-        """Update the text fields based on the given observation."""
+    def update(self) -> None:
+        """Execute one iteration of robot controller and TUI data update."""
 
         observation, status, applied_action = self.robot.update()
 
