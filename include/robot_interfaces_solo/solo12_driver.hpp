de8639183593bb3a761a0e6317b95464addbf123
Felix Kloss <felix.widmaier@tuebingen.mpg.de>
2022-11-18 10:06:32 +0100

    refactor: Move Solo12Config to separate library
    This makes it easier to use it in other libraries.
    
    Also add Eigen to `find_package` and link libraries (also works without
    but seems more correct to add it).

--- include/robot_interfaces_solo/solo12_driver.hpp
+++ include/robot_interfaces_solo/solo12_driver.hpp
@@ -20,57 +20,6 @@
 
 namespace robot_interfaces_solo
 {
-/**
- * @brief Configuration for the Solo12 driver.
- */
-struct Solo12Config
-{
-    /**
-     * @brief Name of the network interface to which the robot is connected
-     * (e.g. "eth0").
-     */
-    std::string network_interface = "";
-
-    /**
-     * @brief Name of the serial port to which the hardware slider is connected.
-     *
-     * This can typically be left empty, in which case the port is
-     * auto-detected.
-     */
-    std::string slider_serial_port = "";
-
-    /**
-     * @brief Maximum current that can be applied to the motors (in Ampere).
-     */
-    double max_motor_current_A = 8.0;
-
-    /**
-     * @brief Offset between home position (=encoder index) and zero position.
-     *
-     * Angles (in radian) between the encoder index and the zero position of
-     * each joint.
-     */
-    Vector12d home_offset_rad = Vector12d::Zero();
-
-    /**
-     * @brief Logger output level.
-     *
-     * One of {"trace", "debug", "info", "warning", "error", "critical", "off"}.
-     * See documentation of spdlog for details.
-     */
-    std::string logger_level = "warning";
-
-    /**
-     * @brief Load configuration from a YAML file.
-     *
-     * @param file Path to the file
-     *
-     * @return Configuration instance.  For parameters not provided in the file
-     *         the default values are kept.
-     */
-    static Solo12Config from_file(const std::filesystem::path &file);
-};
-
 //! Driver to use Solo12
 class Solo12Driver
     : public robot_interfaces::RobotDriver<Solo12Action, Solo12Observation>
--- src/solo12_driver.cpp
+++ src/solo12_driver.cpp
@@ -171,59 +171,6 @@ void Solo12Driver::shutdown()
     }
 }
 
-template <typename T>
-void _set_optional_config_value(const YAML::Node &user_config,
-                                const std::string &name,
-                                T *var)
-{
-    try
-    {
-        if (user_config[name])
-        {
-            *var = user_config[name].as<T>();
-        }
-    }
-    catch (const YAML::Exception &e)
-    {
-        throw std::runtime_error(fmt::format(
-            "FATAL: Failed to load parameter '{}' from configuration file: {}",
-            name,
-            e.what()));
-    };
-}
-
-Solo12Config Solo12Config::from_file(
-    const std::filesystem::path &config_file_name)
-{
-    Solo12Config config;
-    YAML::Node user_config;
-
-    try
-    {
-        user_config = YAML::LoadFile(config_file_name);
-    }
-    catch (const YAML::Exception &e)
-    {
-        throw std::runtime_error(
-            fmt::format("FATAL: Failed to load configuration from '{}': {}",
-                        config_file_name.string(),
-                        e.what()));
-    }
-
-    _set_optional_config_value(
-        user_config, "network_interface", &config.network_interface);
-    _set_optional_config_value(
-        user_config, "slider_serial_port", &config.slider_serial_port);
-    _set_optional_config_value(
-        user_config, "max_motor_current_A", &config.max_motor_current_A);
-    _set_optional_config_value(
-        user_config, "home_offset_rad", &config.home_offset_rad);
-    _set_optional_config_value(
-        user_config, "logger_level", &config.logger_level);
-
-    return config;
-}
-
 FakeSolo12Driver::FakeSolo12Driver(const Solo12Config &config) : config_(config)
 {
     // initialise logger and set level based on config
